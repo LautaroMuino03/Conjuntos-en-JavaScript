1b24bac7f974d3ee3db4aa60733691f47f49e726
Lautaro Muiño <117776336+LautaroMuino03@users.noreply.github.com>
2023-02-24 12:50:01 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,12 @@
+const set = ["Lautaro Muiño", "Fernando Muiño", "Sofia Muiño", "Monica Avalos"]
+
+const miSet = new Set(set);
+
+console.log(set);
+console.log(miSet);
+
+miSet.add("Lautaro Muiño");
+console.log(miSet);
+
+miSet.add("JavaScript");
+console.log(miSet);
